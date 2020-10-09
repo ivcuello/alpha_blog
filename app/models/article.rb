cc6c66b630d1fc46c5f80d1424a606ac4a94fe5a
ivcuello <ivcuello@gmail.com>
2020-10-08 20:47:36 -0400

    Analyzing functionality

---
+++ app/models/article.rb
@@ -0,0 +1,9 @@
+class Article < ApplicationRecord
+  # Non Null Validations
+  validates :title, presence: true
+  validates :description, presence: true
+
+  # Article length validations
+  validates :title, length: { minimum: 6, maximum: 100 }
+  validates :description, length: { minimum: 10, maximum: 300 }
+end
