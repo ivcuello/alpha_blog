ab5e18dbaf4f2df0d4f7d55d888c3a1d1dbbf3cc
ivcuello <ivcuello@gmail.com>
2020-10-21 20:38:46 -0400

    DRY View & reources from scratch

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,55 @@
+class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @articles = Article.all
+  end
+
+  def show
+    # Show article
+  end
+
+  def new
+    # New article form
+    @article = Article.new
+  end
+
+  def edit
+  end
+
+  def create
+    # Creates an article
+    @article = Article.new(article_params)
+    if @article.save
+      flash[:notice] = "Aticles was created successfully"
+      redirect_to article_path(@article)
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    if @article.update(article_params)
+      flash[:notice] = "Aticles was updated successfully"
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @article.destroy
+    redirect_to articles_path
+  end
+
+  private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :description)
+  end
+
+end
