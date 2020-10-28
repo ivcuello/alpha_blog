be464f84211f28b5de5a19140a778efd755e3321
ivcuello <ivcuello@gmail.com>
2020-10-27 21:01:13 -0400

    Adding user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,19 @@
 class User < ApplicationRecord
+  # Non Null attributes
+  validates :email, presence: true
+  validates :username, presence: true
+  
+  # Attributes length
+  validates :email, length: { maximum: 105 }
+  validates :username, length: { minimum: 3, maximum: 25 }
+  
+  # Unique attributes
+  validates :email, uniqueness: { case_sensitive: false }
+  validates :username, uniqueness: { case_sensitive: false }
+
+  # RegExp validations
+  validates :email, format: { 
+    with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i,
+    message: "Only allows a valid email format" 
+  }
 end
