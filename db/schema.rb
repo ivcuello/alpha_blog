c7e93e7edf817af1bc7aaac805c661292c317149
ivcuello <ivcuello@gmail.com>
2020-10-27 22:29:31 -0400

    Updating schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_28_010655) do
+ActiveRecord::Schema.define(version: 2020_10_28_011127) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
